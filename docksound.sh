7ecf6ee755e16d0b44c4a4a15ab77cbf2dd51e42
Rich Greene <22133496+RGreeneRI@users.noreply.github.com>
2020-10-17 12:40:06 -0400

    Instance detection

--- docksound.sh
+++ docksound.sh
@@ -7,22 +7,33 @@ USERNAME='johnsmith'
 USERID='9999'
 PID=$(echo $$)
 
+# Log dock detection
+echo "$(date) - PID:$PID Dock detected" >> /tmp/DockPlugEvent.log
+
+# Terminate script if an instance is already running
+#echo $PID
+ps aux | grep 'docksound.sh' | grep -v -e 'grep' -e "$PID"
+if [[ $? -gt 0 ]]
+then
+	echo "$(date) - PID:$PID No other instance detected, proceeding" >> /tmp/DockPlugEvent.log
+else
+	echo "$(date) - PID:$PID Another instance detected, exiting." >> /tmp/DockPlugEvent.log
+	exit 0
+fi
+
 export PULSE_RUNTIME_PATH="/run/user/$USERID/pulse/"
 
-# Log dock detection
-echo "$(date) - [PID:$PID] - Dock detected" >> /tmp/DockPlugEvent.log
-
 # Wait for things to settle after dock is plugged in
-sleep 5
+sleep 2
 
 # Check to see if desired sound devices are already set, and EXIT without changing IF TRUE
 sudo -u $USERNAME -E pacmd stat | grep $SINK_NAME &&
 	sudo -u $USERNAME -E pacmd stat | grep $SOURCE_NAME &&
-	echo "$(date) - [PID:$PID] - Desired Sound device already active" >> /tmp/DockPlugEvent.log &&
+	echo "$(date) - PID:$PID Desired Sound device already active, exiting" >> /tmp/DockPlugEvent.log &&
 	exit 0
 
 # Change sound Source and Sink to the Docking Station sound device if NOT already set
-echo "$(date) - [PID:$PID] - Firing Sound device switch" >> /tmp/DockPlugEvent.log
+echo "$(date) - PID:$PID Firing Sound device switch" >> /tmp/DockPlugEvent.log
 sudo -u $USERNAME -E pacmd set-default-source "SOURCE_NAME"
 sudo -u $USERNAME -E pacmd set-default-sink "$SINK_NAME"
-echo "$(date) - [PID:$PID] - Fired Sound device switch" >> /tmp/DockPlugEvent.log
+echo "$(date) - PID:$PID Fired Sound device switch" >> /tmp/DockPlugEvent.log
